cc32d8b6fd78bf58337a187e15c29b0b68da1692
yajur12 <123368004+yajur12@users.noreply.github.com>
2025-08-27 00:19:21 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,23 +9,30 @@ import userRoute from './routes/users.js';
 import authRoute from './routes/auth.js';
 import reviewRoute from './routes/reviews.js';
 import bookingRoute from './routes/bookings.js';
-import paymentRoute from './routes/payment.js';
-
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 8000;
-const corsOptions = {
-    origin: "https://voyage-vista-y8xm.vercel.app/", // Allow requests from this origin
-    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
-  };
 
+const corsOptions = {
+    origin: true, // This will reflect the request origin
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Access-Control-Allow-Credentials'],
+    exposedHeaders: ['Content-Range', 'X-Content-Range'],
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    maxAge: 86400 // Increase preflight cache time to 24 hours
+};
+
+app.options('*', cors(corsOptions));
 
 // database connection
 mongoose.set('strictQuery', false);
-const connect =() => {
+const connect = async() => {
     try{
-         mongoose.connect(process.env.MONGO_URL,{
+        
+        await mongoose.connect(process.env.MONGO_URL,{
             useNewUrlParser:true,
             useUnifiedTopology:true,
         });
@@ -36,25 +43,20 @@ const connect =() => {
     }
 }
 
-app.use(express.json());
-// app.use(cors({
-//     origin: 'http://localhost:3000', // The frontend's URL (if your frontend is running on this port)
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow these HTTP methods
-//     allowedHeaders: ['Content-Type', 'Authorization'], // Allow these headers
-//   }));
+// middleware
 app.use(cors(corsOptions));
+app.use(express.json());
 app.use(cookieParser());
+
+// Enable pre-flight requests for all routes
+
+
+// routes
 app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/tours", tourRoute);
 app.use("/api/v1/users", userRoute);
 app.use("/api/v1/review", reviewRoute);
 app.use("/api/v1/booking", bookingRoute);
-app.use("/api/v1/payment", paymentRoute);
-
-app.get('/', (req, res) => {
-    res.send('Welcome to the Tour and Travel API!');
-  });
-  
 
 app.listen(port, () => {
     connect();
